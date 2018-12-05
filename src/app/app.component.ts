0cbc7371bd4fc60f41ed7afff720e1246f1e4e50
R Nandyala <shekar2906@gmail.com>
2018-12-04 22:27:55 -0500

    adding todo model and adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { TodoModel } from '../models/todo.model';
+import { Itodo } from '../interfaces/todo.interface';
 import { TodoService } from '../services/todoService/todo.service';
+import { TodoModel } from '../models/todo.model';
 
 
 @Component({
@@ -11,19 +12,26 @@ import { TodoService } from '../services/todoService/todo.service';
   providers: [TodoService]
 })
 export class AppComponent implements OnInit {
-  todolist: TodoModel[];
-  newItem = '';
+  todolist: Itodo[];
+  newTodo: TodoModel = {
+    id: 0,
+    name: ''
+  };
   constructor(private _TodoService: TodoService) { }
   addToList(addlist: NgForm) {
     const List: any = this.todolist || [];
-    const maxId = List.length > 0 ? List.reduce(function (prev, curr) {
+    const maxId = List.length > 1 ? List.reduce(function (prev, curr) {
       return prev.id > curr.id ? prev.id : curr.id;
-    }) : 0;
-    List.push({ id: maxId + 1, value: addlist.value.createInput });
+    }) : List.length === 0 ? 0 : 1;
+    this.newTodo = {
+      id: maxId + 1,
+      name: addlist.value.name
+    };
+    List.push(this.newTodo);
     addlist.resetForm();
   }
   ngOnInit() {
-    this._TodoService.getResumeDetails()
+    this._TodoService.getTodoList()
       .subscribe(response => this.todolist = response);
   }
   deleteTodo(event) {
--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,3 +1,4 @@
-export interface TodoModel {
-    todoList:  any [];
+export class TodoModel {
+    id: number;
+    name: string;
 }
